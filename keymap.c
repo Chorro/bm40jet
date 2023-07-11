0cc53bf83d10ecf185a8779d163ff39e93c07680
Juan J Chorro Bergillo <jjchorrobe@gmail.com>
2023-07-12 01:28:28 +0200

    feat: replace _______ by XXXXXXX (NOOP)

--- keymap.c
+++ keymap.c
@@ -36,37 +36,37 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     LT4_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, KC_BSPC,
     LCTLESC,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT,
     KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, KC_RSFT,
-    _______, _______, _______, KC_LGUI,     MO2,      KC_SPC     , LT3_ENT, KC_RGUI, _______, _______, _______
+    XXXXXXX, XXXXXXX, XXXXXXX, KC_LGUI,     MO2,      KC_SPC     , LT3_ENT, KC_RGUI, XXXXXXX, XXXXXXX, XXXXXXX
 ),
 
 
 [_ARROWS] = LAYOUT_planck_mit(
-    OSMM   , KC_EJCT, _______, _______, _______, _______, _______, _______,   KC_UP, _______, _______, KC_TRNS,
-    _______, KC_LSFT, KC_LCTL, KC_LALT, KC_RGUI, _______, _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______,
-    _______, _______, _______, _______, _______, KC_VOLD, KC_VOLU, _______, _______, _______, _______, _______,
-    _______, _______, _______, _______, KC_TRNS,     KC_TRNS     , _______, _______, _______, _______, KC_MUTE
+    OSMM   , KC_EJCT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,   KC_UP, XXXXXXX, XXXXXXX, KC_TRNS,
+    XXXXXXX, KC_LSFT, KC_LCTL, KC_LALT, KC_RGUI, XXXXXXX, XXXXXXX, KC_LEFT, KC_DOWN, KC_RGHT, XXXXXXX, XXXXXXX,
+    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_VOLD, KC_VOLU, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_TRNS,     KC_TRNS     , XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_MUTE
 ),
 
 
 [_SYMBOLS] = LAYOUT_planck_mit(
      KC_GRV, KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_MINS,
-    _______, _______, _______, _______, _______, _______, _______, _______,  KC_EQL, KC_LBRC, KC_RBRC, KC_NUHS,
-    KC_TRNS, _______, _______, _______, _______, _______, KC_TILD, _______, _______, _______, _______, KC_TRNS,
-    _______, _______, _______, KC_TRNS,     MO5,     KC_TRNS     , _______, _______, _______, _______, _______
+    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  KC_EQL, KC_LBRC, KC_RBRC, KC_NUHS,
+    KC_TRNS, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_TILD, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_TRNS,
+    XXXXXXX, XXXXXXX, XXXXXXX, KC_TRNS,     MO5,     KC_TRNS     , XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
 ),
 
 [_NUMPAD] = LAYOUT_planck_mit(
-    _______, _______, _______, _______, _______, _______, KC_PPLS,    KC_1,    KC_2,    KC_3, KC_PMNS, KC_TRNS,
-    _______, _______, _______, _______, _______, _______, KC_PAST,    KC_4,    KC_5,    KC_6, KC_PSLS, _______,
-    _______, _______, _______, _______, _______, _______, _______,    KC_7,    KC_8,    KC_9, KC_PEQL, _______,
-    _______, _______, _______, _______, _______,     KC_TRNS     ,   KC_P0, KC_PDOT, _______, _______, _______
+    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_PPLS,    KC_1,    KC_2,    KC_3, KC_PMNS, KC_TRNS,
+    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_PAST,    KC_4,    KC_5,    KC_6, KC_PSLS, XXXXXXX,
+    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,    KC_7,    KC_8,    KC_9, KC_PEQL, XXXXXXX,
+    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,     KC_TRNS     ,   KC_P0, KC_PDOT, XXXXXXX, XXXXXXX, XXXXXXX
 ),
 
 [_ADJUST] = LAYOUT_planck_mit(
       KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,  KC_F12,
-    RGB_MOD, RGB_HUI, RGB_SAI, RGB_VAI, RGB_SPI, _______, _______, KC_MRWD, KC_MPLY, KC_MFFD, _______, _______,
-   RGB_RMOD, RGB_HUD, RGB_SAD, RGB_VAD, RGB_SPD, _______, _______, _______, _______, _______, _______, _______,
-    RGB_TOG, _______, _______, _______, KC_TRNS,     KC_TRNS     , KC_TRNS, _______, _______, _______,  QK_BOOT
+    RGB_MOD, RGB_HUI, RGB_SAI, RGB_VAI, RGB_SPI, XXXXXXX, XXXXXXX, KC_MRWD, KC_MPLY, KC_MFFD, XXXXXXX, XXXXXXX,
+   RGB_RMOD, RGB_HUD, RGB_SAD, RGB_VAD, RGB_SPD, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+    RGB_TOG, XXXXXXX, XXXXXXX, XXXXXXX, KC_TRNS,     KC_TRNS     , KC_TRNS, XXXXXXX, XXXXXXX, XXXXXXX,  QK_BOOT
 )
 
 };
